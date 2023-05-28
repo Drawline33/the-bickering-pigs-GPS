59db794f72af4a300793d20de12dec07dc0faef3
Drawline33 <134034541+Drawline33@users.noreply.github.com>
2023-05-28 01:25:00 -0400

    ----Update----
    5/27.23--- updated the program to allow a back ground and icons for future upgrades to this project. i must admit the baked image loads faster on startup.

---
+++ TBPGPSV1.13.ino
@@ -0,0 +1,466 @@
+//This file is a mixture of 3 or more examples combined into one you will need the following required files
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+// -TintGPS++ can be found https://github.com/mikalhart/TinyGPSPlus
+// -MCUFRIEND_kbv can be found https://github.com/prenticedavid/MCUFRIEND_kbv
+// -SdFat can be found https://github.com/greiman/SdFat 
+#include <Adafruit_GFX.h> // Hardware-specific library
+#include <MCUFRIEND_kbv.h>
+//gps drivers and decoder
+#include <TinyGPS++.h>
+//sd card includes
+#include <SPI.h>             // f.k. for Arduino-1.5.2
+#include "DHT.h"
+#define USE_SDFAT
+#include <SdFat.h>           // Use the SdFat library
+SoftSpiDriver<12, 11, 13> softSpi; //Bit-Bang on the Shield pins SDFat.h v2
+SdFat SD;
+#define SD_CS SdSpiConfig(10, DEDICATED_SPI, SD_SCK_MHZ(50), &softSpi)
+
+
+TinyGPSPlus gps; // create gps object
+File myFile;
+
+MCUFRIEND_kbv tft;
+
+
+#define DHTPIN 23 //define your DHT switch pin.
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+#define PALETTEDEPTH   8     // support 256-colour Palette
+
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+String Latitude, Longitude, Altitude, Speed, Temp, Hum, day, month, year, hour, minute, second, Date, Time, Data;
+
+// work in line numbers.  Font height in ht
+int16_t ht = 16, top = 3, line, lines = 1, scroll;
+DHT dht(DHTPIN, DHTTYPE);
+
+struct Image {
+  const char* path;
+  int x;
+  int y;
+};
+//if you are selecting images from the root of the file you can remove "/Icons" and
+//leave "/yourfilename.bmp" other wise change Icons To your folder name and
+Image images[] = {
+  {"/Icons/gpsBg1.bmp", 0, 0},   // background image at (0, 0)
+  {"/Icons/image1.bmp", 10, 20},  // sat count at (100, 50)
+  {"/Icons/Latlonicon11.bmp", 20, 110},  // lat/long icon (200, 100)
+  {"/Icons/mph11.bmp", 20, 215},  //
+  {"/Icons/tempF2.bmp", 20, 282},
+  {"/Icons/Hum-icon11.bmp", 150, 280}//
+};
+
+int numImages = sizeof(images) / sizeof(images[0]);
+void setup(){
+ 
+    
+  Serial.begin(57600);
+  Serial3.begin(9600);
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+  myFile = SD.open("GPS_data.txt", FILE_WRITE);
+  if (myFile) {
+    myFile.println( "Latitude, Longitude, Altitude, Speed, Temp, Hum, Date and Time \r\n");
+    myFile.close();
+
+  } else {
+    Serial.println("error opening GPS_data.txt");
+  }
+   // connect gps sensor
+    dht.begin();
+    tft.reset();
+    uint16_t id = tft.readID();
+    tft.begin(id);
+    tft.setRotation(45);     //Portrait
+    tft.fillScreen(WHITE);
+    tft.setTextColor(BLACK);
+    tft.setTextSize(2);     // System font is 8 pixels.  ht = 8*2=16
+    
+
+    showBMP(images[0].path, images[0].x, images[0].y); // Display image at specified position
+    showBMP(images[1].path, images[1].x, images[1].y);
+    showBMP(images[2].path, images[2].x, images[2].y);
+    showBMP(images[3].path, images[3].x, images[3].y);
+    showBMP(images[4].path, images[4].x, images[4].y);
+    showBMP(images[5].path, images[5].x, images[5].y);    
+    tft.setCursor(63, 20);
+    tft.print("The Bickering Pigs GPS v1.13");
+    tft.setCursor(60,110);
+    tft.println("LAT: ");
+    tft.setCursor(60,130);
+    tft.println("LON: ");
+    tft.setCursor(8,160);
+    tft.println("Altitude: ");
+     
+
+}
+
+
+void loop()
+{ 
+
+    gpsReadings();
+    tempRead();
+   
+    
+  line++;
+   
+}
+
+void TempSensor(void) {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  } // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F")); 
+  
+  delay(5000);
+}
+
+
+void tempRead(){
+   float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    tft.setCursor(0,240);
+    tft.println(F("Failed to read from DHT sensor!"));
+    return;
+  }else{ // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+  tft.setTextColor(BLACK, WHITE);
+    //TempSensor(); //use this function if you want to print to serial. if screen is not displaying use this to test sensor.
+    tft.setCursor(110, (scroll + top) * ht);
+    if (++scroll >= lines) scroll = 0;
+    tft.vertScroll(top * ht, lines * ht, (scroll) * ht);
+    //location of humidity value print
+    tft.setCursor(190,295);
+    tft.println(h);
+    // temp reading 
+    tft.setCursor(70,295);
+    tft.println(f);
+  }
+  }
+
+
+void gpsReadings(){
+   while(Serial3.available()){ // check for gps data
+    if(gps.encode(Serial3.read()))// encode gps data
+    { 
+     obtain_data();
+    //Latitude
+    Serial.print("Latitude: ");
+    Serial.println(gps.location.lat(),6);
+    //Longitude
+    Serial.print("Longitude: ");
+    Serial.println(gps.location.lng(),6); 
+    //Altitude
+    Serial.print("Altitude: "); 
+    Serial.println(gps.altitude.feet());
+    //Speed
+    Serial.print("Speed: "); 
+    Serial.println(gps.speed.mph());
+    // Number of satellites connected
+    Serial.print("Number of Sats connected: "); 
+    Serial.println(gps.satellites.value()); 
+    
+    //Latitude 
+    tft.setCursor(110, (scroll + top) * ht);
+    if (++scroll >= lines) scroll = 0;
+    tft.vertScroll(top * ht, lines * ht, (scroll) * ht);
+    tft.setTextColor(BLACK, WHITE);
+    tft.setCursor(115,110);
+    tft.println(gps.location.lat(),6);
+    //Longitude
+    tft.setCursor(115,130);
+    tft.println(gps.location.lng(),6);
+    //Altitude
+    tft.setCursor(140,160);
+    tft.println(gps.altitude.feet());
+    //Speed
+    tft.setCursor(90,230);
+    tft.println(gps.speed.mph());
+    // Number of satellites connected
+    tft.setCursor(30,20);
+    tft.println(gps.satellites.value());
+    tft.setTextColor(WHITE,BLACK);
+    delay(5000);
+    line++;
+   }
+   
+  }
+  
+  }
+ void obtain_data()
+{
+  
+  if (gps.location.isValid())
+  {
+    Latitude = gps.location.lat();
+    Longitude = gps.location.lng();
+    Altitude = gps.altitude.meters();
+    Speed = gps.speed.mph();
+    Temp = dht.readTemperature(true);
+    Hum = dht.readHumidity();
+  }
+  else
+  {
+    Serial.println("Location is not available");
+  }
+  
+  if (gps.date.isValid())
+  {
+    month = gps.date.month();
+    day = gps.date.day();
+    year = gps.date.year();
+    Date = day + "-" + month + "-" + year;
+     Serial.println(Date);
+  }
+  else
+  {
+    Serial.print("Date Not Available");
+  }
+ 
+  Serial.print("Time: ");
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) ;
+    hour = gps.time.hour();
+    if (gps.time.minute() < 10);
+    minute = gps.time.minute();
+    if (gps.time.second() < 10) ;
+    second = gps.time.second();
+    Time = hour + ":" + minute + ":" + second;
+  Serial.println(Time);
+  }
+  else
+  {
+    Serial.print("Time Not Available");
+  }
+  
+  Data = Latitude + ", " + Longitude + ", " + Altitude + ", " + Speed + ", " + Temp + ", " + Hum+ ", " + Date + ", " + Time;
+  Serial.print("Save data: ");
+  Serial.println(Data);
+  myFile = SD.open("GPS_data.txt", FILE_WRITE);
+
+  if (myFile) {
+    Serial.print("GPS logging to GPS_data.txt...");
+    myFile.println(Data);
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    Serial.println("error opening GPS_data.txt");
+  } 
+ Serial.println();
+ delay(10000);
+}
+
+
+#define BMPIMAGEOFFSET 54
+
+#define BUFFPIXEL      20
+
+uint16_t read16(File& f) {
+    uint16_t result;         // read little-endian
+    f.read(&result, sizeof(result));
+    return result;
+}
+
+uint32_t read32(File& f) {
+    uint32_t result;
+    f.read(&result, sizeof(result));
+    return result;
+}
+
+uint8_t showBMP(char *filename, int x, int y)
+{
+    File bmpFile;
+    int bmpWidth, bmpHeight;    // W+H in pixels
+    uint8_t bmpDepth;           // Bit depth (currently must be 24, 16, 8, 4, 1)
+    uint32_t bmpImageoffset;    // Start of image data in file
+    uint32_t rowSize;           // Not always = bmpWidth; may have padding
+    uint8_t sdbuffer[3 * BUFFPIXEL];    // pixel in buffer (R+G+B per pixel)
+    uint16_t lcdbuffer[(1 << PALETTEDEPTH) + BUFFPIXEL], *palette = NULL;
+    uint8_t bitmask, bitshift;
+    boolean flip = true;        // BMP is stored bottom-to-top
+    int w, h, row, col, lcdbufsiz = (1 << PALETTEDEPTH) + BUFFPIXEL, buffidx;
+    uint32_t pos;               // seek position
+    boolean is565 = false;      //
+
+    uint16_t bmpID;
+    uint16_t n;                 // blocks read
+    uint8_t ret;
+
+    if ((x >= tft.width()) || (y >= tft.height()))
+        return 1;               // off screen
+
+    bmpFile = SD.open(filename);      // Parse BMP header
+    bmpID = read16(bmpFile);    // BMP signature
+    (void) read32(bmpFile);     // Read & ignore file size
+    (void) read32(bmpFile);     // Read & ignore creator bytes
+    bmpImageoffset = read32(bmpFile);       // Start of image data
+    (void) read32(bmpFile);     // Read & ignore DIB header size
+    bmpWidth = read32(bmpFile);
+    bmpHeight = read32(bmpFile);
+    n = read16(bmpFile);        // # planes -- must be '1'
+    bmpDepth = read16(bmpFile); // bits per pixel
+    pos = read32(bmpFile);      // format
+    if (bmpID != 0x4D42) ret = 2; // bad ID
+    else if (n != 1) ret = 3;   // too many planes
+    else if (pos != 0 && pos != 3) ret = 4; // format: 0 = uncompressed, 3 = 565
+    else if (bmpDepth < 16 && bmpDepth > PALETTEDEPTH) ret = 5; // palette 
+    else {
+        bool first = true;
+        is565 = (pos == 3);               // ?already in 16-bit format
+        // BMP rows are padded (if needed) to 4-byte boundary
+        rowSize = (bmpWidth * bmpDepth / 8 + 3) & ~3;
+        if (bmpHeight < 0) {              // If negative, image is in top-down order.
+            bmpHeight = -bmpHeight;
+            flip = false;
+        }
+
+        w = bmpWidth;
+        h = bmpHeight;
+        if ((x + w) >= tft.width())       // Crop area to be loaded
+            w = tft.width() - x;
+        if ((y + h) >= tft.height())      //
+            h = tft.height() - y;
+
+        if (bmpDepth <= PALETTEDEPTH) {   // these modes have separate palette
+            //bmpFile.seek(BMPIMAGEOFFSET); //palette is always @ 54
+            bmpFile.seek(bmpImageoffset - (4<<bmpDepth)); //54 for regular, diff for colorsimportant
+            bitmask = 0xFF;
+            if (bmpDepth < 8)
+                bitmask >>= bmpDepth;
+            bitshift = 8 - bmpDepth;
+            n = 1 << bmpDepth;
+            lcdbufsiz -= n;
+            palette = lcdbuffer + lcdbufsiz;
+            for (col = 0; col < n; col++) {
+                pos = read32(bmpFile);    //map palette to 5-6-5
+                palette[col] = ((pos & 0x0000F8) >> 3) | ((pos & 0x00FC00) >> 5) | ((pos & 0xF80000) >> 8);
+            }
+        }
+
+        // Set TFT address window to clipped image bounds
+        tft.setAddrWindow(x, y, x + w - 1, y + h - 1);
+        for (row = 0; row < h; row++) { // For each scanline...
+            // Seek to start of scan line.  It might seem labor-
+            // intensive to be doing this on every line, but this
+            // method covers a lot of gritty details like cropping
+            // and scanline padding.  Also, the seek only takes
+            // place if the file position actually needs to change
+            // (avoids a lot of cluster math in SD library).
+            uint8_t r, g, b, *sdptr;
+            int lcdidx, lcdleft;
+            if (flip)   // Bitmap is stored bottom-to-top order (normal BMP)
+                pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+            else        // Bitmap is stored top-to-bottom
+                pos = bmpImageoffset + row * rowSize;
+            if (bmpFile.position() != pos) { // Need seek?
+                bmpFile.seek(pos);
+                buffidx = sizeof(sdbuffer); // Force buffer reload
+            }
+
+            for (col = 0; col < w; ) {  //pixels in row
+                lcdleft = w - col;
+                if (lcdleft > lcdbufsiz) lcdleft = lcdbufsiz;
+                for (lcdidx = 0; lcdidx < lcdleft; lcdidx++) { // buffer at a time
+                    uint16_t color;
+                    // Time to read more pixel data?
+                    if (buffidx >= sizeof(sdbuffer)) { // Indeed
+                        bmpFile.read(sdbuffer, sizeof(sdbuffer));
+                        buffidx = 0; // Set index to beginning
+                        r = 0;
+                    }
+                    switch (bmpDepth) {          // Convert pixel from BMP to TFT format
+                        case 32:
+                        case 24:
+                            b = sdbuffer[buffidx++];
+                            g = sdbuffer[buffidx++];
+                            r = sdbuffer[buffidx++];
+                            if (bmpDepth == 32) buffidx++; //ignore ALPHA
+                            color = tft.color565(r, g, b);
+                            break;
+                        case 16:
+                            b = sdbuffer[buffidx++];
+                            r = sdbuffer[buffidx++];
+                            if (is565)
+                                color = (r << 8) | (b);
+                            else
+                                color = (r << 9) | ((b & 0xE0) << 1) | (b & 0x1F);
+                            break;
+                        case 1:
+                        case 4:
+                        case 8:
+                            if (r == 0)
+                                b = sdbuffer[buffidx++], r = 8;
+                            color = palette[(b >> bitshift) & bitmask];
+                            r -= bmpDepth;
+                            b <<= bmpDepth;
+                            break;
+                    }
+                    lcdbuffer[lcdidx] = color;
+
+                }
+                tft.pushColors(lcdbuffer, lcdidx, first);
+                first = false;
+                col += lcdidx;
+            }           // end cols
+        }               // end rows
+        tft.setAddrWindow(0, 0, tft.width() - 1, tft.height() - 1); //restore full screen
+        ret = 0;        // good render
+    }
+    bmpFile.close();
+    return (ret);
+}
